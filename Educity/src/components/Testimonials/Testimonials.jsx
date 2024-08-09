8c51f883021c51f4fde279566775d862bd3e0411
claire <clairemwas20@gmail.com>
2024-08-09 11:01:19 +0300

    sytling

--- Educity/src/components/Testimonials/Testimonials.jsx
+++ Educity/src/components/Testimonials/Testimonials.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './Testimonials.css'
 import next_icon from '../../assets/edusity_assets/next-icon.png'
 import back_icon from '../../assets/edusity_assets/back-icon.png'
@@ -8,11 +8,70 @@ import user_3 from '../../assets/edusity_assets/user-3.png'
 import user_4 from '../../assets/edusity_assets/user-4.png'
 
 const Testimonials = () => {
+  const slider = useRef();
+  let tx = 0;
+
+  const slideFoward =() =>{
+    if(tx >-50){
+      tx -=25;
+    }
+    slider.current.style.transform=`translateX(${tx}%)`;
+  }
+  const slideBackward =() =>{
+    
+  }
   return (
     <div className='testimonial'>
-        <img src={next_icon} alt='' className='next_iconb'/>
-        <img src={back_icon} alt='' className='back_iconb'/>
-        <div className="slider"></div>
+        <img src={next_icon} alt='' className='next_iconb'onClick={slideFoward}/>
+        <img src={back_icon} alt='' className='back_iconb'onClick={slideBackward}/>
+        <div className="slider">
+          <ul ref ={"slider"}>
+            <li>
+              <div className="slide">
+                <div className="user_info">
+                  <img src={user_1} alt=''/>
+                  <div>
+                    <h3>William Johnson</h3>
+                    <span>Edisity USA</span>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="slide">
+                <div className="user_info">
+                  <img src={user_2} alt=''/>
+                  <div>
+                    <h3>Partricis Seci</h3>
+                    <span>Edisity USA</span>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="slide">
+                <div className="user_info">
+                  <img src={user_3} alt=''/>
+                  <div>
+                    <h3>James Johnson</h3>
+                    <span>Edisity USA</span>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="slide">
+                <div className="user_info">
+                  <img src={user_4} alt=''/>
+                  <div>
+                    <h3>William James</h3>
+                    <span>Edisity USA</span>
+                  </div>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
     </div>
   )
 }
